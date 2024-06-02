e215c5e979eb44d8bcb5519fa91d40e1546f85f8
Lau <160362151+lea-devv@users.noreply.github.com>
2024-06-02 10:58:12 +0200

    Ready for release

--- Previous/flask_test.py
+++
@@ -1,7 +0,0 @@
-from restart.file_request import Recent_Files
-
-recent_files_list = Recent_Files("C:/Users/frago\Documents/testbkup", 100)
-files_returned = recent_files_list.get_recent_files()
-for x in range(len(files_returned)):
-    print(files_returned[x])
-
